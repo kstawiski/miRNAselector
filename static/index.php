3085acde52f4efc07685b364635fc6745dd82917
Konrad Stawiski <konrad@konsta.com.pl>
2020-03-22 14:39:15 +0000

    zmiana monitora na top -n 1

--- static/index.php
+++ static/index.php
@@ -1,29 +1,21 @@
 <html>
   <head>
-    <meta name="generator"
-    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
     <title>miRNAselector</title>
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
-    <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
     integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
-    <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
     integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <!-- Custom styles for this template -->
     <link href="css/starter-template.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"
-    integrity="sha256-MAgcygDRahs+F/Nk5Vz387whB4kSK9NXlDN3w58LLq0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js" integrity="sha256-MAgcygDRahs+F/Nk5Vz387whB4kSK9NXlDN3w58LLq0=" crossorigin="anonymous"></script>
     <script src="js/cpufunctions.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-1.8.3.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/ui/1.9.2/jquery-ui.js" type="text/javascript"></script>
@@ -55,24 +47,14 @@
       <div class="starter-template">
         <img src="logo.png" width="80%" />
       </div>
-      <div class="starter-template">
         <h2>Introduction</h2>
         <p>Welcome to 
         <b>miRNAselector</b> - the software intended to find the best biomarker signiture based on NGS and qPCR data.</p>
-      </div>
-      <div class="starter-template">
         <h2>Upload your data</h2>
-        <pre>
-<?php system("ps -ef"); ?>
-</pre>
-        <pre>
-<?php system("df -h"); ?>
-</pre>
-      </div>
-      <div class="starter-template">
+        <pre><?php system("ps -ef"); ?></pre>
+        <pre><?php system("df -h"); ?></pre>
         <h2>System Monitor</h2>
 		<pre><?php passthru('top -n 1'); ?></pre>
-      </div>
     </div>
     <hr />
     <footer class="footer">
