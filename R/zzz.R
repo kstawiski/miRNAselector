39e3639c626226d33904e3293650a8b4239197b9
Konrad Stawiski <konrad@konsta.com.pl>
2020-05-12 21:45:56 +0000

    dodany onLoad

---
+++ R/zzz.R
@@ -0,0 +1,25 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to miRNAselector! For more details go to https://kstawiski.github.io/miRNAselector/")
+}
+
+.onLoad <- function(libname, pkgname) {
+  require("BiocManager")
+  
+  # Normalne paczki
+  packages <- c("devtools","plyr","dplyr","edgeR","epiDisplay","rsq","MASS","Biocomb","caret","dplyr", "roxygen2", "plotly", "rJava", "mice", "MatchIt", "kableExtra", "reticulate",
+                       "pROC","ggplot2","DMwR", "doParallel", "Boruta", "spFSR", "varSelRF", "stringr", "psych", "C50", "randomForest",
+                       "foreach","data.table", "ROSE", "deepnet", "gridExtra", "stargazer","gplots","My.stepwise","snow", "sva", "Biobase",
+                       "calibrate", "ggrepel", "networkD3", "VennDiagram","RSNNS", "kernlab", "car", "PairedData",
+                       "profileR","classInt","kernlab","xgboost", "keras", "tidyverse", "tibble","tidyr", "rpart", "party", "mgcv", "GDCRNATools",
+                       "imputeMissings", "visdat", "naniar", "stringr", "doSNOW", "R.utils", "TCGAbiolinks", "GDCRNATools", "kableExtra", "VIM", "mice", "MatchIt", "XML", "rmarkdown", "xtable", "ComplexHeatmap","circlize")
+  if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  BiocManager::install(setdiff(packages, rownames(installed.packages())), ask = F)  }
+
+  # Paczki z githuba
+  library(devtools)
+  if("bounceR" %in% rownames(installed.packages()) == FALSE) { devtools::install_github("STATWORX/bounceR") }
+  if("cutpointr" %in% rownames(installed.packages()) == FALSE) { devtools::install_github("Thie1e/cutpointr") }
+  if("ggbiplot" %in% rownames(installed.packages()) == FALSE) { devtools::install_github("vqv/ggbiplot") }
+
+  invisible(rownames(installed.packages()))
+  }
