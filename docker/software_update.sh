12df736d2271e58ef751589f091ffc8c8b24375b
Konrad Stawiski <konrad@konsta.com.pl>
2020-05-12 15:53:45 +0000

    asd

--- docker/software_update.sh
+++ docker/software_update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "Starting update..." | tee /update.log
-date | tee -a /update.log
+echo "Starting update..." 2>&1 | tee /update.log
+date 2>&1 | tee -a /update.log
 cd /miRNAselector/miRNAselector
-git reset --hard | tee -a /update.log
-git clean -df | tee -a /update.log
-git pull | tee -a /update.log
-Rscript -e 'library(devtools); BiocManager::install(ask = F); install_github("STATWORX/bounceR"); install_github("Thie1e/cutpointr"); install_github("vqv/ggbiplot"); install_github("kstawiski/miRNAselector", force=TRUE)' | tee -a /update.log
-echo "The update is finished. Please go back to the app." | tee -a /update.log
+git reset --hard 2>&1 | tee -a /update.log
+git clean -df 2>&1 | tee -a /update.log
+git pull 2>&1 | tee -a /update.log
+Rscript -e 'library(devtools); BiocManager::install(ask = F); install_github("STATWORX/bounceR"); install_github("Thie1e/cutpointr"); install_github("vqv/ggbiplot"); install_github("kstawiski/miRNAselector", force=TRUE)' 2>&1 | tee -a /update.log
+echo "The update is finished. Please go back to the app." 2>&1 | tee -a /update.log
