3b1c9c3562d9f6edfc780d91a958097c22dfdc7a
Konrad Stawiski <konrad@konsta.com.pl>
2020-05-12 14:59:13 +0000

    kolejny fix updatera

--- docker/software_update.sh
+++ docker/software_update.sh
@@ -5,5 +5,5 @@ cd /miRNAselector/miRNAselector
 git reset --hard | tee -a /update.log
 git clean -df | tee -a /update.log
 git pull | tee -a /update.log
-Rscript -e 'devtools::install_github("kstawiski/miRNAselector", force=TRUE)' | tee -a /update.log
+Rscript -e 'library(devtools); BiocManager::install(ask = F); install_github("kstawiski/miRNAselector", force=TRUE)' | tee -a /update.log
 echo "The update is finished. Please go back to the app." | tee -a /update.log
