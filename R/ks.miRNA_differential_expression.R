a5549f06333915859b4749bc0655ce89dbc46d8d
Konrad Stawiski <konrad@konsta.com.pl>
2020-06-06 11:12:53 +0000

    foldchage fix for qPCR

--- R/ks.miRNA_differential_expression.R
+++ R/ks.miRNA_differential_expression.R
@@ -7,11 +7,13 @@
 #'
 #' @param ttpm_pofiltrze matrix of log-transformed TPM-normalized miRNAs counts with miRNAs placed in `ttpm` design (i.e. columns and cases placed as rows)
 #' @param klasy vector describing label for each case. It should contain only "Cancer" and "Control" labeles!!!!
-#'
+#' @param mode use 'logtpm' for log(TPM) data or 'deltact' for qPCR deltaCt values. This parameters sets how the fold-change is calculated. 
+#' 
+#' 
 #' @return Data frame with results.
 #'
 #' @export
-ks.miRNA_differential_expression = function(ttpm_pofiltrze, klasy)
+ks.miRNA_differential_expression = function(ttpm_pofiltrze, klasy, mode = "logtpm")
 {
   suppressMessages(library(plyr))
   suppressMessages(library(dplyr))
@@ -43,9 +45,9 @@ ks.miRNA_differential_expression = function(ttpm_pofiltrze, klasy)
     #wyniki[i,"jakieszero"] = ifelse(sum(ttpm_pofiltrze[,i] == 0) > 0, "tak", "nie")
 
     # Średnia i SD
-    wyniki[i,"mean logtpm"] = mean(ttpm_pofiltrze[,i])
-    wyniki[i,"median logtpm"] = median(ttpm_pofiltrze[,i])
-    wyniki[i,"SD logtpm"] = sd(ttpm_pofiltrze[,i])
+    wyniki[i,paste0("mean ",mode)] = mean(ttpm_pofiltrze[,i])
+    wyniki[i,paste0("median ",mode)] = median(ttpm_pofiltrze[,i])
+    wyniki[i,paste0("SD ",mode)] = sd(ttpm_pofiltrze[,i])
 
     # Cancer
     tempx = ttpm_pofiltrze[klasy == "Cancer",]
@@ -61,6 +63,7 @@ ks.miRNA_differential_expression = function(ttpm_pofiltrze, klasy)
 
     # DE
     temp = t.test(ttpm_pofiltrze[,i] ~ as.factor(klasy))
+    if(mode == "logtpm") {
     fc = (temp$estimate[1] - temp$estimate[2])
     wyniki[i,"log10FC (subtr estim)"] = fc
     wyniki[i,"log10FC"] = wyniki[i,"cancer mean"] - wyniki[i,"control mean"]
@@ -68,13 +71,27 @@ ks.miRNA_differential_expression = function(ttpm_pofiltrze, klasy)
 
     revfc = (temp$estimate[2] - temp$estimate[1])
     wyniki[i,"reversed_log10FC"] = revfc
-    wyniki[i,"reverse_log2FC"] = wyniki[i,"reversed_log10FC"] / log10(2)
+    wyniki[i,"reversed_log2FC"] = wyniki[i,"reversed_log10FC"] / log10(2)
     #wyniki[i,"log2FC"] = log2(fc)
+    }
+
+    if(mode == "deltact") {
+    fc = 2 ^ (temp$estimate[1] - temp$estimate[2])
+    wyniki[i,"FC"] = fc
+    wyniki[i,"log10FC"] = log10(fc)
+    wyniki[i,"log2FC"] = log2(fc)
+
+    revfc = 2 ^ (temp$estimate[2] - temp$estimate[1])
+    wyniki[i,"reversed_log10FC"] = log10(fc)
+    wyniki[i,"reversed_log2FC"] = log2(fc)
+    #wyniki[i,"log2FC"] = log2(fc)
+    }
+
     wyniki[i,"p-value"] = temp$p.value
   }
   wyniki[,"p-value Bonferroni"] = p.adjust(wyniki$`p-value`, method = "bonferroni")
   wyniki[,"p-value Holm"] = p.adjust(wyniki$`p-value`, method = "holm")
-  wyniki[,"-log10(p-value Bonferroni)"] = -log10(p.adjust(wyniki$`p-value`, method = "bonferroni"))
+  # wyniki[,"-log10(p-value Bonferroni)"] = -log10(p.adjust(wyniki$`p-value`, method = "bonferroni"))
   wyniki[,"p-value BH"] = p.adjust(wyniki$`p-value`, method = "BH")
 
   wyniki$miR = as.character(wyniki$miR)
--- vignettes/setup.R
+++ vignettes/setup.R
@@ -14,7 +14,7 @@ packages = c("remotes","devtools","parallel","rlang","ps","roxygen2", "plotly", 
                        "foreach","data.table", "ROSE", "deepnet", "gridExtra", "stargazer","gplots","My.stepwise","snow", "sva", "Biobase",
                        "calibrate", "ggrepel", "networkD3", "VennDiagram","RSNNS", "kernlab", "car", "PairedData",
                        "profileR","classInt","kernlab","xgboost", "keras", "tidyverse", "cutpointr","tibble","tidyr",
-                       "rpart", "party", "mgcv", "GDCRNATools", "rJava", "cutpointr",
+                       "rpart", "party", "mgcv", "GDCRNATools", "rJava", "cutpointr", "HTqPCR", "nondetects",
                        "imputeMissings", "visdat", "naniar", "stringr", "R.utils", "TCGAbiolinks", "GDCRNATools",
                        "kableExtra", "VIM", "mice", "MatchIt", "XML", "rmarkdown", "xtable", "ComplexHeatmap","circlize",
                        "BiocStyle","magick", "BiocCheck","cluster","tidyselect","ellipsis")
