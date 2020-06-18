a5553d4e9d7038bc8bbe3e975bd93ba3f2a5d4a2
Konrad Stawiski <konrad@konsta.com.pl>
2020-06-19 00:21:53 +0200

    update dokumentacji

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   # options(rgl.useNULL = TRUE)
-  ks.setup(keras = FALSE, msg = FALSE)
-
-
+  # ks.setup(keras = FALSE, msg = FALSE)
   packageStartupMessage("\n\nWelcome to miRNAselector!\nAuthors: Konrad Stawiski M.D. (konrad@konsta.com.pl) and Marcin Kaszkowiak.\n\n\nYou can start with running ks.setup() to be sure that everything is properlly installed.\nFor more details go to https://kstawiski.github.io/miRNAselector/\n")
 }
 
