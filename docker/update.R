0bae4b7d13df7afc3054f753b22ff99a9d50784f
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-01 00:39:57 +0000

    update fix

--- docker/update.R
+++ docker/update.R
@@ -1,5 +1,4 @@
 library(devtools)
 source_url("https://raw.githubusercontent.com/kstawiski/miRNAselector/master/vignettes/setup.R")
-install_github("kstawiski/miRNAselector", force = TRUE) # Install our package.
+install_github('kstawiski/mirnaselector', upgrade = 'never') # Install our package.
 library(miRNAselector)
-ks.setup()
