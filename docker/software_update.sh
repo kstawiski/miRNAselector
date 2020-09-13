d19659f6b44fb7e40cdb052ffbc80e2fb72445f9
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-13 18:10:49 +0000

    version

--- docker/software_update.sh
+++ docker/software_update.sh
@@ -5,6 +5,7 @@ cd /miRNAselector/miRNAselector
 git reset --hard 2>&1 | tee -a /update.log
 git clean -df 2>&1 | tee -a /update.log
 git pull 2>&1 | tee -a /update.log
-Rscript --verbose -e 'devtools::install_github("kstawiski/miRNAselector", upgrade = "never");' 2>&1 | tee -a /update.log
+Rscript -e 'devtools::install_github("kstawiski/miRNAselector", upgrade = "never");' 2>&1 | tee -a /update.log
 # Rscript --verbose -e 'source("/miRNAselector/miRNAselector/docker/keras.R");' 2>&1 | tee -a /update.log
+git rev-parse --short HEAD | tee /version.txt
 echo "The update is finished. Please go back to the app." 2>&1 | tee -a /update.log
