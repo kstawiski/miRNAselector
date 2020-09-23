faffbb2351443fe6d66597a80c07ec6a4c24fd4c
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-23 19:29:33 +0000

    a

--- docker/feature_selection.R
+++ docker/feature_selection.R
@@ -1,8 +1,5 @@
 options(warn = -1)
 if(file.exists("task.log")) { file.remove("task.log") }
-con <- file("task.log")
-sink(con, append=TRUE)
-sink(con, append=TRUE, type="message")
 
 library(miRNAselector)
 suppressMessages(library(foreach))
@@ -13,7 +10,7 @@ suppressMessages(library(doParallel))
 mm = read.csv("selected_methods.csv")
 m = as.numeric(mm$m) # which methods to check?
 
-cl <- makePSOCKcluster(useXDR = TRUE, 3) # We do not recommend using more than 5 threads, beacuse some of the methods inhereditly use multicore processing.
+cl <- makePSOCKcluster(useXDR = TRUE, 3,  outfile="task.log") # We do not recommend using more than 5 threads, beacuse some of the methods inhereditly use multicore processing.
 registerDoParallel(cl)
 
 foreach(i = m) %dopar%
@@ -23,17 +20,15 @@ foreach(i = m) %dopar%
   max_iterations = readLines("var_max_iterations.txt", warn = F)
   timeout_sec = readLines("var_timeout_sec.txt", warn = F)
   # setwd("/miRNAselector/miRNAselector/vignettes") # change it you to your working directory
-  con <- file("task.log")
-  sink(con, append=TRUE)
-  sink(con, append=TRUE, type="message")
   ks.miRNAselector(m = i, max_iterations = 1, stamp = "fs", debug = F, # we set debug to false (to make the package smaller), you may also want to change stamp to something meaningful, max_iterations was set to 1 to recude the computational time.. in real life scenarios it is resonable to use at least 10 iterations.
                   prefer_no_features = prefer_no_features, # Few methods are filter rather than wrapper methods, thus requires the maximum number of maximum features.    
                   timeout_sec = timeout_sec) # We don't want to wait eternity in this tutorial, just 10 minutes. Timeout is useful for complicated methods. Depending on your CPU 2 days may be reasonable for larger projects. Note that some methods cannot be controled with timeout parameter.
-  sink() 
-  sink(type="message")
 }
 stopCluster(cl)
 
+con <- file("task.log")
+sink(con, append=TRUE)
+sink(con, append=TRUE, type="message")
 prefer_no_features = readLines("var_prefer_no_features.txt", warn = F)
 selected_sets_of_miRNAs = ks.merge_formulas(max_miRNAs = prefer_no_features)
 
