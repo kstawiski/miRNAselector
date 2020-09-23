0668c83ace4c9cc241d5e10c912f90ed4ad357b5
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-23 23:39:29 +0000

    simplified

--- docker/feature_selection.R
+++ docker/feature_selection.R
@@ -5,7 +5,7 @@ sink(con, append=TRUE)
 sink(con, append=TRUE, type = "message")
 
 library(miRNAselector)
-# suppressMessages(library(foreach))
+suppressMessages(library(foreach))
 # suppressMessages(library(doParallel))
 # suppressMessages(library(parallel))
 # suppressMessages(library(doParallel))
@@ -19,20 +19,25 @@ prefer_no_features = readLines("var_prefer_no_features.txt", warn = F)
 max_iterations = readLines("var_max_iterations.txt", warn = F)
 timeout_sec = readLines("var_timeout_sec.txt", warn = F)
 
-#foreach(i = m) %dopar%
-for(i in m)
-{
-  suppressMessages(library(miRNAselector))
-  
-  # setwd("/miRNAselector/miRNAselector/vignettes") # change it you to your working directory
-  ks.miRNAselector(m = i, max_iterations = 1, stamp = "fs", debug = F, # we set debug to false (to make the package smaller), you may also want to change stamp to something meaningful, max_iterations was set to 1 to recude the computational time.. in real life scenarios it is resonable to use at least 10 iterations.
+ks.miRNAselector(m = m, max_iterations = max_iterations, stamp = "fs", debug = F, # we set debug to false (to make the package smaller), you may also want to change stamp to something meaningful, max_iterations was set to 1 to recude the computational time.. in real life scenarios it is resonable to use at least 10 iterations.
                   prefer_no_features = prefer_no_features, # Few methods are filter rather than wrapper methods, thus requires the maximum number of maximum features.    
-                  timeout_sec = timeout_sec) # We don't want to wait eternity in this tutorial, just 10 minutes. Timeout is useful for complicated methods. Depending on your CPU 2 days may be reasonable for larger projects. Note that some methods cannot be controled with timeout parameter.
-}
+                  timeout_sec = timeout_sec)
+
+
+# foreach(i = m) %do%
+#for(i in m)
+# {
+#   suppressMessages(library(miRNAselector))
+#   prefer_no_features = readLines("var_prefer_no_features.txt", warn = F)
+#   max_iterations = readLines("var_max_iterations.txt", warn = F)
+#   timeout_sec = readLines("var_timeout_sec.txt", warn = F)
+#   # setwd("/miRNAselector/miRNAselector/vignettes") # change it you to your working directory
+#   ks.miRNAselector(m = i, max_iterations = max_iterations, stamp = "fs", debug = F, # we set debug to false (to make the package smaller), you may also want to change stamp to something meaningful, max_iterations was set to 1 to recude the computational time.. in real life scenarios it is resonable to use at least 10 iterations.
+#                   prefer_no_features = prefer_no_features, # Few methods are filter rather than wrapper methods, thus requires the maximum number of maximum features.    
+#                   timeout_sec = timeout_sec) # We don't want to wait eternity in this tutorial, just 10 minutes. Timeout is useful for complicated methods. Depending on your CPU 2 days may be reasonable for larger projects. Note that some methods cannot be controled with timeout parameter.
+# }
 # stopCluster(cl)
 
-
-prefer_no_features = readLines("var_prefer_no_features.txt", warn = F)
 selected_sets_of_miRNAs = ks.merge_formulas(max_miRNAs = prefer_no_features)
 
 cat("[miRNAselector: TASK COMPLETED]")
