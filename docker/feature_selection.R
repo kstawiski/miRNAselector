b69437afbadd4eb74860a92cdfe423155e965530
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-24 00:00:05 +0000

    a

--- docker/feature_selection.R
+++ docker/feature_selection.R
@@ -15,9 +15,9 @@ library(miRNAselector)
 mm = read.csv("selected_methods.csv")
 m = as.numeric(mm$m) # which methods to check?
 
-prefer_no_features = readLines("var_prefer_no_features.txt", warn = F)
-max_iterations = readLines("var_max_iterations.txt", warn = F)
-timeout_sec = readLines("var_timeout_sec.txt", warn = F)
+prefer_no_features = as.numeric(readLines("var_prefer_no_features.txt", warn = F))
+max_iterations = as.numeric(readLines("var_max_iterations.txt", warn = F))
+timeout_sec = as.numeric(readLines("var_timeout_sec.txt", warn = F))
 
 ks.miRNAselector(m = m, max_iterations = max_iterations, stamp = "fs", debug = F, # we set debug to false (to make the package smaller), you may also want to change stamp to something meaningful, max_iterations was set to 1 to recude the computational time.. in real life scenarios it is resonable to use at least 10 iterations.
                   prefer_no_features = prefer_no_features, # Few methods are filter rather than wrapper methods, thus requires the maximum number of maximum features.    
