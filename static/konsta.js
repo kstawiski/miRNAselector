40c542ee373efad11bb0132f560d7c9615d73086
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-26 14:49:48 +0200

    v1.0

--- static/konsta.js
+++ static/konsta.js
@@ -69,3 +69,11 @@ return {
 
 })(jQuery);
 
+/* PRELOADER */
+$( "body" ).prepend( '<div id="preloader"><div class="spinner-sm spinner-sm-1" id="status"> </div></div>' );
+$(window).on('load', function() { // makes sure the whole site is loaded
+  $('#status').fadeOut(); // will first fade out the loading animation
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(350).css({'overflow':'visible'});
+})
+
