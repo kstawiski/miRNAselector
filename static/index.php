702a957e293d63147e7dc5ebc702f49ac617efab
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-26 14:04:20 +0200

    v1.0

--- static/index.php
+++ static/index.php
@@ -31,8 +31,6 @@ if($pid != "") { header("Location: /inprogress.php"); }
     <meta name="description" content="miRNAselector - a tool for selecting great miRNA biomarkers." />
     <meta name="author" content="Konrad Stawiski (konrad@konsta.com.pl)" />
     <link rel="stylesheet" href="css/starter-template.css" />
-        <script src="https://cdn.jsdelivr.net/npm/jquery-bez@1.0.11/src/jquery.bez.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
     <script src="all.min.js"
         integrity="sha256-MAgcygDRahs+F/Nk5Vz387whB4kSK9NXlDN3w58LLq0=" crossorigin="anonymous"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -113,33 +111,6 @@ if($pid != "") { header("Location: /inprogress.php"); }
 </head>
 
 <body>
-        <div id="preloader">
-        <div class="p">LOADING</div>
-        </div>
-
-        <script>
-
-        paceOptions = {
-        ajax: true,
-        document: true,
-        eventLag: false
-        };
-
-        Pace.on('done', function() {
-        $('.p').delay(500).animate({top: '30%', opacity: '0'}, 3000, $.bez([0.19,1,0.22,1]));
-
-
-        $('#preloader').delay(1500).animate({top: '-100%'}, 2000, $.bez([0.19,1,0.22,1]));
-
-        TweenMax.from(".title", 2, {
-             delay: 1.8,
-                  y: 10,
-                  opacity: 0,
-                  ease: Expo.easeInOut
-            })
-       });
-
-      </script>
 
     <div class="container">
         <div class="starter-template">
