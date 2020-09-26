19e5c34a30e573e398e7a116eaae94d5aeb70a35
Konrad Stawiski <konrad@konsta.com.pl>
2020-09-26 14:51:41 +0200

    v1.0

--- static/index.php
+++ static/index.php
@@ -44,6 +44,13 @@ if($pid != "") { header("Location: /inprogress.php"); }
     </script>
     <script type="text/javascript">
 
+    $( "body" ).prepend( '<div id="preloader"><div class="spinner-sm spinner-sm-1" id="status"></div></div>' );
+$(window).on('load', function() { // makes sure the whole site is loaded
+  $('#status').fadeOut(); // will first fade out the loading animation
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(350).css({'overflow':'visible'});
+})
+
     $(".btn-success").click(function (event) {
         waitingDialog.show('Processing.. Please wait...');
             });
